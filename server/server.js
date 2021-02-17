a0c0fdf2591e506376373234b00e86795ef320bb
Jerry smith <jcsmith115@outlook.com>
2021-02-16 21:48:53 -0500

    Made changes to deploy

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const PORT = env.port || 3000
+const PORT = process.env.PORT || 3000
 
 const publicPath = path.join( __dirname, '..', 'public')
 
